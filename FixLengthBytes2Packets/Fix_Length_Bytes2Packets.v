e6289fbca4f8bd4b3245186b6eebd463946129d7
CNLHC <2463765697@qq.com>
2018-12-15 00:54:20 +0800

    添加打包器TCL脚本，修改实现中硬编码的部分

--- FixLengthBytes2Packets/Fix_Length_Bytes2Packets.v
+++ FixLengthBytes2Packets/Fix_Length_Bytes2Packets.v
@@ -1,69 +1,67 @@
 `timescale 1 ps / 1 ps
-module Fix_Length_Bytes2Packets (
+module Fix_Length_Bytes2Packets#(
+parameter SYMBOL_PER_PACKET=256,
+parameter BYTES_PER_SYMBOL=8,
+parameter BITS_PER_BYTES=8
+)
+(
     // Clock and Reset
-		input  wire        clock_clk,              //     clock_1.clk
-		input  wire        reset_reset,            //       reset.reset
+		input  wire        clock_clk,              
+		input  wire        reset_reset,            
     //Avalon-ST Sink
-		input  wire [7:0]  asi_in0_data,           //     asi_in0.data
-		output wire asi_in0_ready,          //            .ready
-		input  wire        asi_in0_valid,          //            .valid
+		input  wire        [BITS_PER_BYTES-1:0]  asi_in0_data,           
+		output wire        asi_in0_ready,          
+		input  wire        asi_in0_valid,          
     //Avalon-ST Source
-		output wire [16:0] aso_out0_data,          //    aso_out0.data
-		input  wire        aso_out0_ready,         //            .ready
-		output reg         aso_out0_valid,         //            .valid
-		output reg         aso_out0_startofpacket, //            .startofpacket
-		output reg         aso_out0_endofpacket,   //            .endofpacket
-        output wire        aso_out0_empty
+		output wire        [BYTES_PER_SYMBOL*BITS_PER_BYTES:0] aso_out0_data,          
+		output reg         aso_out0_valid,         
+		output reg         aso_out0_startofpacket, 
+		output reg         aso_out0_endofpacket,   
 	);
-    reg [12:0]tSymbolCounter; 
-    reg tPacketState;
-    reg [7:0]realData;
-    reg [7:0]imagData;
-    assign aso_out0_data[16:9] = realData;
-    assign aso_out0_data[8:1] = imagData;
-    assign aso_out0_data[0] = 1;
-    reg tInnerPacketReady;
 
-    assign asi_in0_ready = tInnerPacketReady &aso_out0_ready;
+    reg [12:0] tSymbolCounter; //MAX-8191,suitbale for this application.
+    reg [BYTES_PER_SYMBOL/BITS_PER_BYTES:0]  tBytesCounter;
+    reg tPacketState;
+
     assign aso_out0_empty = 0;
+    assign asi_in0_ready =1 ;
 
     always @(posedge reset_reset or posedge clock_clk) begin
         if(reset_reset) begin
             tSymbolCounter<=0;
+            tBytesCounter<=0;
             tPacketState<=0;
-            tInnerPacketReady<=1;
         end
         else begin 
-            
-
             if(aso_out0_startofpacket)
                 aso_out0_startofpacket<=0;
             if(aso_out0_endofpacket)begin
                 aso_out0_valid<=0;
                 aso_out0_endofpacket<=0;
-                tSymbolCounter<=0;
-                tPacketState<=0;
-                tInnerPacketReady<=1;
             end
-            
-
 
             if(asi_in0_valid)begin
-                if(tSymbolCounter>=1023 )begin
-                    aso_out0_endofpacket<=1;
-                end
+                tBytesCounter<=tBytesCounter+1;
+                aso_out0_data[(BYTES_PER_SYMBOL-tBytesCounter)*BITS_PER_BYTES-1-:BITS_PER_BYTES]<=asi_in0_data;
+            end
+            if(tBytesCounter>BYTES_PER_SYMBOL-1)begin
                 if(!tPacketState)begin
                     aso_out0_startofpacket<=1;
                     tPacketState<=1;
                 end
-                realData <= asi_in0_data;
-                imagData <= 0;
                 tSymbolCounter<=tSymbolCounter+1;
+                tBytesCounter<=0;
                 aso_out0_valid<=1;
-            end
-            else
-                aso_out0_valid<=0;
+                if(tSymbolCounter>SYMBOL_PER_PACKET-1)begin 
+                    tSymbolCounter<=0;
+                    tPacketState<=0;
+                    aso_out0_endofpacket<=1;
 
+                end
+            end
+            else begin
+                aso_out0_valid<=0;
+            end
         end
     end
 endmodule
